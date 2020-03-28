0c5fdf75da9e78879deb65bff41bca80c38eebce
mansi-goyal194 <mansi.goyal_cs18@gla.ac.in>
2020-03-28 16:14:05 +0530

    problem3 done

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -6,6 +6,7 @@
  */
 package problem3.main;
 
+import problem3.Student;
 import problem3.myqueue.MyPriorityQueue;
 
 import java.util.Scanner;
@@ -16,6 +17,19 @@ public class MyMain {
     public static void main(String[] args) {
         MyPriorityQueue queue = new MyPriorityQueue();
         Scanner sc = new Scanner(System.in);
-        System.out.println("");
+        System.out.println("enter no. of students you want to enter");
+        int n = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i <n ; i++) {
+            System.out.println("enter the Student name: ");
+            String name = sc.nextLine();
+            System.out.println("enter the roll number of this student: ");
+            int rollNo = sc.nextInt();
+
+            Student student = new Student(name,rollNo);
+            queue.enqueue(student);
+            sc.nextLine();
+        }
+        queue.show();
     }
 }
